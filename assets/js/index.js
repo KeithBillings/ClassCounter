520b8cd7e3b0b59d8a200c9ea3cfc713de072a8c
Keith Billings <63262663+KeithBillings@users.noreply.github.com>
2020-07-02 17:30:58 -0700

    addIssacListJS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+// List Population 
+
 const russianNames = $('#russianNames')
 const maryusRecording =$('#maryusRecording')
 
@@ -8,14 +10,37 @@ function buildHtmlList(array,list) {
 
     $(list).append(
       `<div class="row">
-      <div class="col-sm-4">${i + 1}</div>
-      <div class="col-sm-4">${name}</div>
-      <div class="col-sm-4">${count}</div>
-    </div>`
+        <div class="col-sm-4">${i + 1}</div>
+        <div class="col-sm-4">${name}</div>
+        <div class="col-sm-4">${count}</div>
+      </div>`
     )
   }
 }
 
 buildHtmlList(russianNamesArray, russianNames);
 buildHtmlList(remindMaryusToRecord, maryusRecording);
-// console.log(russianNames[0].name)
+
+// Issac Population 
+
+const issacList = $('#issacList');
+const issacCount = 10;
+
+
+function buildIssacList (count) {
+
+  let issacImgList = ``;
+  for (i = 0; i < count; i++){
+    issacImgList = issacImgList + `<img src="./assets/images/mutemics.jpg" alt="Issac" />`
+  }
+
+  issacList.append(
+    `<h1>Issac Saying "Mute Your Mics!"</h1>
+    <div class="issacPics">
+      ${issacImgList}
+    </div>
+    <h5>${count} times</h5>`
+  )
+}
+
+buildIssacList(issacCount);
